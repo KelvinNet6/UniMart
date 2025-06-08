01724bd463140bf8adc7a0ccc86901f186de7960
Krlvin <kelvin.net6@gmail.com>
2025-06-08 17:58:48 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 export const supabase = createClient(
-  'https://your-project-id.supabase.co',
-  'public-anon-key'
+   "https://honrgtrvzpymssmlbcbn.supabase.co",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvbnJndHJ2enB5bXNzbWxiY2JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyODMzOTEsImV4cCI6MjA2Mzg1OTM5MX0.dcQNVrulRZxGvyeYX6Wq8VtJKi3OYpYVzirR5jk8axw"
 );
